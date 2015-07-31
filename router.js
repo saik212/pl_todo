a2e0db29a4ec50df4e2d379751f0e0b8c332b9c2
Sai Kandala <kandala.sai@gmail.com>
2015-07-31 17:26:15 -0400

    basic REST API to CRUD todos

--- router.js
+++ router.js
@@ -3,39 +3,76 @@ var router = express.Router();
 
 
 // Pseudo DB setup
-var todoLists = [{
-	id: 1,
-	title: 'Movies to watch',
-	items: [
-		{title: 'Gone with the Wind', watched: false},
-		{title: 'The Matrix', watched: false},
-		{title: 'A Nightmare Before Christmas', watched: false},
-		{title: 'The Room', watched: false},
-		{title: 'Raising Arizona', watched: false}
-	]
-}];
+var todoList = [
+		{id:1, desc: 'Go skydiving', complete: false},
+		{id:2, desc: 'Watch The Matrix', complete: false},
+		{id:3, desc: 'Watch A Nightmare Before Christmas', complete: false},
+		{id:4, desc: 'Make a todo list', complete: false},
+		{id:5, desc: 'Watch Raising Arizona', complete: false}
+];
+
+// var to keep track of ID for added items (needed for lack of DB)
+var totalItems = 5;
+
+// search helper method
+// get params ID, and search for element
+// o(n) used for convenience, but is not optimal
+var searchList = function (id) {
+	var todoItemIdx = "Item not found";
+	for (var i=0; i<todoList.length; i++) {
+		if (todoList[i].id === id) {
+			todoItemIdx = i;
+			break
+		}
+	};
+
+	return todoItemIdx;
+}
 
 // set up routes
+
+
 router.get('/', function (req, res) {
 	res.render('index');
 });
 
+
+// Basic CRUD for todos
+
 router.route('/api/todos')
 	.get(function (req, res) {
-		res.json(todoLists);
+		res.json(todoList);
 	})
 
 	.post(function (req, res) {
-		res.json('So you wanna make a new list, eh?');
+		totalItems+=1;
+		var newItem = {id: totalItems, desc:'New todo '+ totalItems, complete: false};
+		todoList.push(newItem);
+		res.json(newItem);
 	});
 
-// router.get('/api/todos', function (req, res) {
-// 	res.json(todoLists);
-// });
+router.route('/api/todos/:id')
+	.get(function (req, res) {
+		var reqId = parseInt(req.params.id);
+			
+		res.json(todoList[searchList(reqId)]);
+	})
 
-// router.post('/api/todos', function (req, res) {
-// 	res.json('So you wanna make a new list, eh?');
-// })
+	.put(function (req, res) {
+		var reqId = parseInt(req.params.id);
+		var item = todoList[searchList(reqId)];
+		res.json('Updating todo: ' + item.desc);
+	})
+
+	.delete(function (req, res){
+		var reqId = parseInt(req.params.id);
+		var delIdx = searchList(reqId);
+		var delItem = todoList[delIdx];
+
+		todoList.splice(delIdx, 1);
+
+		res.json(delItem);
+	});
 
 
 
